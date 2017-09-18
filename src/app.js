df9608f2c619e9fdc2fc5922376fbe6a48bea291
Michael Blanton <mblanton@gmail.com>
2017-09-17 17:21:36 -0700

    added initial code for firebase support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    console.log('componentWillMount: Initializing firebase');
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCQFsfp_0Fz3wlrRTtCzpDeincicXAO60s',
+      authDomain: 'auth-8919d.firebaseapp.com',
+      databaseURL: 'https://auth-8919d.firebaseio.com',
+      projectId: 'auth-8919d',
+      storageBucket: 'auth-8919d.appspot.com',
+      messagingSenderId: '674993967952'
+    });
+  }
+
   render() {
     return (
       <View>
