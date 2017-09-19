2394b89e2443c329aea75c15df68c38e6c010db8
Michael Blanton <mblanton@gmail.com>
2017-09-18 17:45:45 -0700

    Fixed display bug

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { authenticated: null }
+  state = { authenticated: null };
+
   componentWillMount() {
     console.log('componentWillMount: Initializing firebase');
     firebase.initializeApp({
@@ -26,18 +27,22 @@ class App extends Component {
   }
 
   // Make the spinner centered on the screen
+  // There's a bug in Flex and we apparently need to wrap these in a
+  // CardSection (https://www.udemy.com/the-complete-react-native-and-redux-course/learn/v4/questions/2121342)
   renderContent() {
     switch (this.state.authenticated) {
       case true:
         return (
+          <CardSection>
             <Button onPress={() => firebase.auth().signOut()}>
               Log Out
             </Button>
+          </CardSection>
         );
       case false:
         return <LoginForm />;
       default:
-        return <Spinner size="large" />;
+        return <CardSection><Spinner size="large" /></CardSection>;
     }
   }
 
